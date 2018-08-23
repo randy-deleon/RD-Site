b3a109c7a5911318a3a9b5b224dce9ca7d8b61e6
Randy De Leon <randy.deleon@outlook.com>
2018-08-23 07:28:43 -0700

    index update

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,22 +1,21 @@
 import React,{Component,Fragment} from 'react';
+import Raven from 'raven-js';
 import Head from './head'
 import GridContainer from './grid-container'
 import Header from '../components/header'
 import Main from '../components/main'
 import Footer from '../components/footer'
 import '../layouts/index.css'
- import Raven from 'raven-js';
 class Template extends Component {
   render() {
+    { process.env.NODE_ENV === 'production' ? Raven.config(`https://069eb3a55db741da8e44e285551621f4@sentry.io/1266601`, { environment: 'production'}).install(): null }
     const {location, children } = this.props;
     let rootPath = `/`;
     if (typeof __PREFIX_PATHS__ !== `undefined` && __PREFIX_PATHS__) {
       rootPath = __PATH_PREFIX__ + `/`;
     }
-
     return (
       <Fragment>
-      { process.env.NODE_ENV === 'production' ? Raven.config(`${process.env.SENTRY_DSN}`, { environment: 'production'}).install(): null }
         <Head />
         <GridContainer>
            {(location.pathname === '/') ? null : <Header nonav />}
